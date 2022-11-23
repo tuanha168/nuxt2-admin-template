3f18cc1d432fdd331d5889c1a6703e690bc817f8
tuanha168 <achicir2@gmail.com>
2022-11-23 17:02:10 +0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,1 @@
-git pull
-docker-compose exec frontend yarn
-docker-compose exec frontend yarn generate
+ssh fpt-fe "cd nuxt2-admin-template; git pull; docker-compose exec frontend yarn; docker-compose exec frontend yarn generate"
